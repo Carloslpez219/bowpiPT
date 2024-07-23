68eff59f8aa3b53ea0838dfd014763a47788652d
Carloslpez219 <56690399+Carloslpez219@users.noreply.github.com>
2024-07-23 14:28:06 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bowpiPT',
+  webDir: 'www'
+};
+
+export default config;
